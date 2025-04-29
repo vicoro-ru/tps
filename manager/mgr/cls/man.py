da8e9074af2f187c8a46be35c027712d4c08550f
Mike <mike@NZXT>
2025-04-29 15:35:23 +0300

    Start Write central tor manager module

---
+++ manager/mgr/cls/man.py
@@ -0,0 +1,6 @@
+class Manager(object):
+	"""docstring for Manager"""
+	def __init__(self, arg):
+		super(Manager, self).__init__()
+		self.arg = arg
+		
---
+++ manager/mgr/main.py
@@ -0,0 +1,12 @@
+from cls.man import Manager
+from time import sleep
+from sys import stdout, stderr
+
+def main ():
+	while True:
+		stdout.write("Claster manager:\n Start try update VPN list")
+		# sleep 1 hour = 3600
+		sleep(15)
+	return 0
+
+main()
